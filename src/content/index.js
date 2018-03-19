c954663341065b4da68c0258ca200b93f43684eb
h-morishita <h-morishita@esol.co.jp>
2018-03-19 18:18:47 +0900

    HTMLをパースして全体の構造をオブジェクト化するようにした。

--- src/content/index.js
+++ src/content/index.js
@@ -1,6 +1,10 @@
 import jQuery from 'jquery'
 const $ = jQuery
 
+// 追加するHTMLタグidのプレフィックス
+const idPrefix = '--pjpex-'
+
+// このスクリプトがロードされたときに表示する（デバッグ用）
 console.log('PJP, content-script!')
 
 // クリップボードに文字列をコピーする
@@ -19,6 +23,154 @@ function execCopy (string) {
 
   document.body.removeChild(temp)
   return result // true なら実行できている falseなら失敗か対応していないか
+}
+
+// かんばんのHTMLをパースして、各要素のHTMLタグのid一覧を返す
+function parseKanban (callback) {
+  let data = {}
+
+  // project id
+  let overview = $('#main-menu').find('.overview')
+  data['project_id'] = $(overview).attr('href').substr(10) // delete '/projects/'
+
+  // story
+  data['stories'] = []
+  let swimlanes = $('[id^=swimlane-')
+  swimlanes.each((i, swimlane) => {
+    try {
+      let featureid = swimlane.id.slice(9) // delete 'swimlane-'
+
+      // ストーリーの情報を格納するdiv
+      let storyBox = $(swimlane).find('.story')[0]
+      $(storyBox).attr('id', `${idPrefix}story_box-${featureid}`)
+
+      // FeatureIDとか表示するdiv
+      let featureHeaderBox = $(storyBox).children()[0]
+      $(featureHeaderBox).attr('id', `${idPrefix}story_header_box-${featureid}`)
+
+      // Featureチケットへのリンク
+      let featureTicketLink = $(storyBox).find('a')[0]
+      $(featureTicketLink).attr('id', `${idPrefix}story_id_link-${featureid}`)
+
+      // ストーリー名
+      let storyName = $(storyBox).find('.subject')[0]
+      $(storyName).attr('id', `${idPrefix}story_name-${featureid}`)
+
+      // make object
+      let story = {
+        id: {
+          value: featureid
+        },
+        name: {
+          value: storyName.innerText,
+          domId: storyName.id
+        },
+        swimlane: {
+          domId: swimlane.id
+        },
+        box: {
+          domId: storyBox.id
+        },
+        headerBox: {
+          domId: featureHeaderBox.id
+        },
+        featureTicketLink: {
+          value: featureTicketLink.innerText,
+          domId: featureTicketLink.id
+        }
+      }
+      data['stories'].push(story)
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+  // status
+  for (let story of data['stories']) {
+    let swimlane = $(`#${story.swimlane.domId}`)
+    let columns = $(swimlane).children()
+
+    // make object
+    let status = [
+      {
+        status: 'opened',
+        box: { domId: columns[1].id }
+      },
+      {
+        status: 'planned',
+        box: { domId: columns[2].id }
+      },
+      {
+        status: 'working',
+        box: { domId: columns[3].id }
+      },
+      {
+        status: 'fixed',
+        box: { domId: columns[4].id }
+      },
+      {
+        status: 'closed',
+        box: { domId: columns[5].id }
+      },
+      {
+        status: 'wontfix',
+        box: { domId: columns[6].id }
+      }
+    ]
+    story['status'] = status
+  }
+
+  // task
+  for (let story of data['stories']) {
+    for (let status of story['status']) {
+      let box = $(`#${status.box.domId}`)[0]
+
+      let taskBoxs = $(box).children()
+
+      // task
+      let tasks = []
+      for (let taskBox of taskBoxs) {
+        let taskid = taskBox.id.slice(6)
+
+        // TaskIDとか表示するdiv
+        let taskHeaderBox = $(taskBox).children()[0]
+        $(taskHeaderBox).attr('id', `${idPrefix}task_header_box-${taskid}`)
+
+        // Taskチケットへのリンク
+        let taskTicketLink = $(taskBox).find('a')[0]
+        $(taskTicketLink).attr('id', `${idPrefix}task_id_link-${taskid}`)
+
+        // タスク名
+        let taskName = $(taskBox).find('.subject')[0]
+        $(taskName).attr('id', `${idPrefix}task_name-${taskid}`)
+
+        // make object
+        let task = {
+          id: {
+            value: taskid
+          },
+          name: {
+            value: taskName.innerText,
+            domId: taskName.id
+          },
+          box: {
+            domId: taskBox.id
+          },
+          headerBox: {
+            domId: taskHeaderBox.id
+          },
+          taskTicketLink: {
+            value: taskTicketLink.innerText,
+            domId: taskTicketLink.id
+          }
+        }
+        tasks.push(task)
+      }
+      status['tasks'] = tasks
+    }
+  }
+
+  callback(data)
 }
 
 // IDをクリップボードにコピーするためのボタンを作成する
@@ -64,7 +216,7 @@ function createIdCopyButton () {
         >
         #
         </button>`
-      $(elem).find('.prevent_edit').before(html)
+      $(elem).find('.prevent_edit').before(html.replace(/\r?\n/g, ' '))
     } catch (error) {
       console.log(error)
     }
@@ -121,7 +273,7 @@ function createIdTitleButton () {
                 vertical-align:bottom;">
           ■
           </button>`
-      $(elem).find('.prevent_edit').after(html)
+      $(elem).find('.prevent_edit').after(html.replace(/\r?\n/g, ' '))
     } catch (error) {
       console.log(error)
     }
@@ -159,7 +311,7 @@ function createOpenChildIdList () {
         >
         ●
         </a>`
-      $(story).find('a').before(html)
+      $(story).find('a').before(html.replace(/\r?\n/g, ' '))
     } catch (error) {
       console.log(error)
     }
@@ -169,8 +321,14 @@ function createOpenChildIdList () {
 // 拡張機能の popup から呼ばれる
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   console.log('content: called')
+
+  parseKanban((data) => {
+    console.log(data)
+  })
   createOpenChildIdList()
   createIdTitleButton()
   createIdCopyButton()
-  sendResponse({msg: 'content: called'})
+  sendResponse({
+    msg: 'content: called'
+  })
 })
